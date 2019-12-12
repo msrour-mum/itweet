8b049eb41a96f9481f6684e07bae4db2e31603a5
Mamoud Srour <msrour@mum.edu>
2019-12-11 23:58:34 -0600

    Setup index page, Internationalization, error page

--- src/main/java/mum/itweet/ItweetApplication.java
+++ src/main/java/mum/itweet/ItweetApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItweetApplication {
-
+public class ItweetApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(ItweetApplication.class, args);
 	}
---
+++ src/main/java/mum/itweet/exceptions/ControllerExceptionAdvice.java
@@ -0,0 +1,17 @@
+package mum.itweet.exceptions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class ControllerExceptionAdvice {
+
+    @ExceptionHandler
+    public String handelException(Exception ex)
+    {
+        System.out.println("Error while execution, [ERROR]: " + ex);
+        return "redirect:/error";
+    }
+}
