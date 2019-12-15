d1dc0ae74a11d38aa0f2b75b7c3e3d965d6f8b1c
Mamoud Srour <msrour@mum.edu>
2019-12-15 02:54:46 -0600

    Enhance WebSecurityConfig, change default success URL

--- src/main/java/mum/itweet/security/WebSecurityConfig.java
+++ src/main/java/mum/itweet/security/WebSecurityConfig.java
@@ -71,16 +71,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/index")
+                .defaultSuccessUrl("/")
                 .failureUrl("/login?error")
                 .and()
                 .logout()
-                .logoutSuccessUrl("/login")
+                .logoutSuccessUrl("/")
                 .and()
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                 //.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                //.and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ;
