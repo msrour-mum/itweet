4293e3706bf0f96fe7a77f4ead7d12e1a5b98107
mohamed <mshehata@mum.edu>
2019-12-14 18:35:56 -0600

    advertisement RestController

---
+++ src/main/java/mum/itweet/service/AdvertisementService.java
@@ -0,0 +1,15 @@
+package mum.itweet.service;
+
+import mum.itweet.model.Advertisement;
+
+import java.util.List;
+
+public interface AdvertisementService {
+
+    public Advertisement create(Advertisement advertisement);
+    public Advertisement update(Advertisement advertisement);
+    public void deleteAdvertisement(long id);
+    public Advertisement getAdvertisementById(long id);
+    public List<Advertisement> getAllAdvertisements();
+
+}
